299c19d6def9bfa4a5c62e4eee2391bf20b1e96a
catdintran <trancatdin86@yahoo.com>
2017-06-07 00:55:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 from econ_util import *
 
 app = Flask(__name__)
@@ -9,7 +10,13 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')  
 
-
+@app.route('/upload')
+def upload():
+  uploaded_files = flask.request.files.getlist("file[]")
+  print uploaded_files
+  for f in uploaded_files:
+    print type(f)
+  return render_template('index.html') 
 
 if __name__ == '__main__':
   app.run()
