221b3ea39d3fff650bae53d370b0acd3877ff442
catdintran <trancatdin86@yahoo.com>
2017-06-07 16:56:35 +0000

    Update jstree_util.js

--- static/jstree_util.js
+++ static/jstree_util.js
@@ -1,8 +1,45 @@
 $(document).ready(function(){
  //  $('#container').jstree();
    draw_jstree();
+   $('#checkbox_btn').click(function(){
+        show_hide_checkbox($(this));
+   });
+   $('#collapse_expand_btn').click(function(){
+        collapse_expand_node($(this));
+   });
+   $('#download_btn').click(function(){
+        download_checked_items();
+   });
  
 });
+
+function download_checked_items(){
+     idList = $('.jstree-clicked').map(function(){
+                    return this.id
+             }).toArray();
+     console.log(idList);
+}
+
+function collapse_expand_node(data){
+   if($(data).text() == 'Expand'){
+      $('#container').jstree('open_all');
+      $(data).text() == 'Collapse';
+   }else{
+      $('#container').jstree('close_all');
+      $(data).text() == 'Expand';
+   }
+} 
+function show_hide_checkbox(data){
+   if($(data).text() == 'Checkbox'){
+         $(data).text() = 'Hide Checkbox';
+         $('.jstree-checkbox').show();
+         $('#download_btn').show();
+   }else{
+         $(data).text() = 'Checkbox';
+         $('.jstree-checkbox').hide();
+         $('#download_btn').hide();
+   }
+} 
 
 function draw_jstree() {
 //   alert('in draw_jstree()')
