ddf0f9164aed39f2109abaa767c407424c8e03f3
catdintran <trancatdin86@yahoo.com>
2017-06-08 18:49:10 +0000

    Create jstree_util.js

--- static/jstree_util.js
+++ static/jstree_util.js
@@ -19,9 +19,9 @@ $(document).ready(function(){
       $('#processing_icon').show();
       return true;
     })
-    $('.jstree-anchor.jstree-hovered').click(function() {
-       console.log($(this));
-    })
+//    $('.jstree-anchor.jstree-hovered').click(function() {
+//       console.log($(this));
+//    })
  
 });
 
