faad705dc7e58760352451cd7d7f4f433bcef700
catdintran <trancatdin86@yahoo.com>
2017-06-12 15:22:12 +0000

    Create econ_util.py

--- econ_util.py
+++ econ_util.py
@@ -32,7 +32,8 @@ def util_process_pdf_file(pdfPath):
 	process_pdftohtml(newpdfPath)
 	
 def process_pdftohtml(pdfPath):
-	filename = pdfPath.split('/')[-1].replace('.txt', '')	
+	filename = pdfPath.split('/')[-1].replace('.pdf', '')
+	filename = filename.replace('.txt', '')
 	output = util_get_html_dir() + filename
 	
 	# convert pdf to html folder
