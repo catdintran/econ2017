efb835d934b0b32255565c2e70eebe58ea4f52b4
catdintran <trancatdin86@yahoo.com>
2017-06-12 14:05:40 +0000

    Create main.py

--- main.py
+++ main.py
@@ -48,10 +48,9 @@ def populate_jstree():
 def display_file():
   print 'calling /display_file'
   filePath = request.json['id']
-  print filePath
-  print filePath
   with open(filePath, 'r') as f:
     text = f.read().replace('\n', '')
+  print text
   return text
 
 if __name__ == '__main__':
