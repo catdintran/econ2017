dddc42ff4b101d11d34a58da6560560648aeea74
catdintran <trancatdin86@yahoo.com>
2017-06-16 18:52:37 +0000

    Update nltk_econ.py

--- nltk_econ.py
+++ nltk_econ.py
@@ -1,1 +1,17 @@
 import nltk
+
+
+
+def textPreprocess(filePath):
+  treebank_tagger = get_treebank_trained_tagger()
+  
+ 
+def get_perceptron_trained_tagger():
+  return nltk.data.load(get_home_dir()+'perceptron-tagger.pickle')
+def get_treebank_trained_tagger():
+  return nltk.data.load(get_home_dir()+'treebank-tagger.pickle')
+  
+def get_home_dir():
+  return '/home/catdt_datascience/app/'
+  
+  
